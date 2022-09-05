e45237564dea36e24a8d047e7c63d762d6bb0e1b
Navan Sami <navan.sami@yahoo.com>
2022-09-05 06:57:40 +0400

    implemented context for Login - set up initial state and reducer to login in user - Provider wrapped around main App

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useReducer } from 'react';
+import axios from 'axios';
+
+export const LoginContext = createContext();
+
+const LoginReducer = (state, action) => {
+    switch(action.type) {
+        case "LOGIN_USER":
+            return { 
+                ...state, 
+                email: action.payload.email,
+                password: action.payload.password,
+                token: action.payload.data.access_token 
+            }
+        default:
+            return state
+    }
+}
+
+
+export function LoginProvider({ children }) {
+    const [state, dispatch] = useReducer(LoginReducer, {
+        token: 1,
+        email: "",
+        password: ""
+    })
+
+    const loginUser = async (user, endpoint) => {
+        console.log(user, endpoint);
+        const res = await axios.post(endpoint, user);
+        const data = await res;
+
+        const payload = { data: data.data, email: user.email, password: user.password }
+        
+        dispatch({ type:"LOGIN_USER", payload })
+    }
+
+    return (
+        <LoginContext.Provider value={{
+            ...state, loginUser
+        }}>
+            { children }
+        </LoginContext.Provider>
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Styles
 import './index.css';
+
+// Components
 import App from './App';
+import { LoginProvider } from './context/LoginContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginProvider>
+      <App />
+    </LoginProvider>
   </React.StrictMode>
 );

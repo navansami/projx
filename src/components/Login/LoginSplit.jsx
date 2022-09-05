8378bd73fa2434ea043cc36d9c13ca7920bb7a31
Navan Sami <navan.sami@yahoo.com>
2022-09-05 07:35:32 +0400

    added presentational component on login page - second split ajacent to form

---
+++ src/components/Login/LoginSplit.jsx
@@ -0,0 +1,21 @@
+
+
+const LoginSplit = () => {
+
+    return (
+        <div className="login-present">
+                <img src={'images/login/illustration.png'} alt="presentation split" className="illustration" />
+                <div className="login-text">
+                    <h1>Enter the Network and start</h1>
+                    <h1>growing your Business!</h1>
+                    
+                    <p>
+                        Profit Paradise is the first Network that gives you all the neccessary to promote and sell products and services talking advantage of Performance Marketing.
+                    </p>
+                </div>
+
+            </div>
+    )
+}
+
+export default LoginSplit;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,14 @@
+
+// Components
 import LoginForm from "../components/Login/LoginForm";
+import LoginSplit from "../components/Login/LoginSplit";
 
 const Login = () => {
 
     return(
         <div className="login">
             <LoginForm />
+            <LoginSplit />
         </div>
     )
 }
